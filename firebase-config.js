430fec5b5f2c8ac080a82001f1e39608a102367a
Gokul1843 <gb600018@gmail.com>
2026-01-23 22:12:26 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// 🔥 Firebase Configuration
-// Replace with your own Firebase project keys
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "000000000",
-  appId: "1:000000:web:000000"
+  apiKey: "AIzaSyArz2brIEgV5X9VEM0w5kM47mrn8inrPdU",
+  authDomain: "schoolhomework-122f7.firebaseapp.com",
+  databaseURL: "https://schoolhomework-122f7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "schoolhomework-122f7",
+  storageBucket: "schoolhomework-122f7.firebasestorage.app",
+  messagingSenderId: "611623855581",
+  appId: "1:611623855581:web:abc247ff42c8d70bd24234"
 };
 
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
