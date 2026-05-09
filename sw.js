374da79890287300030beab0f7eec7f74736b215
Gokul1843 <gb600018@gmail.com>
2026-05-09 06:52:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = "homework-cache-v1";
-
-const urlsToCache = [
-  "./",
-  "./homework.html",
-  "https://html2canvas.hertzen.com/dist/html2canvas.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', () => {
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', () => {
+    self.clients.claim();
 });
